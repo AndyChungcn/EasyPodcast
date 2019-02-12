2b03015c9a074e7813e20436d41b13117074332e
AndyChung <andychung1005@gmail.com>
2019-02-12 10:45:42 +0800

    Initial Commit

---
+++ EasyPodcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyPodcast
+//
+//  Created by 钟汇杭 on 2019/2/12.
+//  Copyright © 2019 钟汇杭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
