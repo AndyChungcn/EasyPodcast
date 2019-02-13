e6967ed06ca4e5be3ca775f0ae2b0f276c3748b2
AndyChung <andychung1005@gmail.com>
2019-02-13 09:02:55 +0800

    MainTabBarController

---
+++ EasyPodcast/MainTabBarController.swift
@@ -0,0 +1,40 @@
+//
+//  MainTabBarController.swift
+//  EasyPodcast
+//
+//  Created by 钟汇杭 on 2019/2/12.
+//  Copyright © 2019 钟汇杭. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = .purple
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+        setupViewControllers()
+    }
+    
+    func setupViewControllers() {
+        viewControllers = [
+            generateNavController(for: ViewController(), title: "Favorite", image: #imageLiteral(resourceName: "favorites")),
+            generateNavController(for: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    // MARK:- Helper Functions
+    
+    fileprivate func generateNavController(for rootViewController: UIViewController, title: String, image: UIImage) -> UINavigationController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+//        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }
+}
+
